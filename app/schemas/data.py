97e533196c9d0e6a3731c649fbfe1e5b4878535c
Kayvan Shah <kayvan.shah@usc.edu>
2023-03-20 01:25:25 -0700

    discarded unused schemas

--- app/schemas/data.py
+++ app/schemas/data.py
@@ -6,21 +6,6 @@ from pydantic import BaseModel, Field
 from app.db.utils import PyObjectId
 
 
-class Data(BaseModel):
-    key: str
-    value: dict | str
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "key": "aqi",
-                "value": {"Name": "example", "Type": "Wind"},
-            }
-        }
-
-
 class DataModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     data: Any
