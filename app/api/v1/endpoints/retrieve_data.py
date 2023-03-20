7238fd761edfed1c7be3767156cbcdee1cd68f0d
Kayvan Shah <kayvan.shah@usc.edu>
2023-03-20 01:26:36 -0700

    added GET root endpoint

--- app/api/v1/endpoints/retrieve_data.py
+++ app/api/v1/endpoints/retrieve_data.py
@@ -10,6 +10,36 @@ router = APIRouter()
 
 
 @router.get(
+    "/.json",
+    status_code=status.HTTP_200_OK,
+    response_description="Sucessfully fetched data",
+)
+async def query_data_root(
+    orderBy: Optional[str | None] = None,
+    limitToFirst: Optional[int | None] = None,
+    limitToLast: Optional[int | None] = None,
+    equalTo: Optional[int | str | None] = None,
+    startAt: Optional[int | str | None] = None,
+    endAt: Optional[int | str | None] = None,
+):
+    """_summary_
+
+    Args:
+        path (str): _description_
+        orderBy (Optional[str  |  None], optional): _description_. Defaults to None.
+        limitToFirst (Optional[int  |  None], optional): _description_. Defaults to None.
+        limitToLast (Optional[int  |  None], optional): _description_. Defaults to None.
+        equalTo (Optional[int  |  str  |  None], optional): _description_. Defaults to None.
+        startAt (Optional[int  |  str  |  None], optional): _description_. Defaults to None.
+        endAt (Optional[int  |  str  |  None], optional): _description_. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
+    return {"Message": "Done"}
+
+
+@router.get(
     "/{path:path}.json",
     status_code=status.HTTP_200_OK,
     response_description="Sucessfully fetched data",
