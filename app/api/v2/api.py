b5803c48a0fce3d7b7c3247f564639a5a6d3943a
Kayvan Shah <kayvan.shah@usc.edu>
2023-03-30 01:15:50 -0700

    import fix

--- app/api/v2/api.py
+++ app/api/v2/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import retrieve_data, save_data
+from app.api.v2.endpoints import retrieve_data, save_data
 
 api_router = APIRouter()
 api_router.include_router(save_data.router, tags=["Save Data"])
