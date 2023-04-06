03a2af1a753677d2c8301fb2fa548a2055b806dd
Kayvan Shah <kayvan.shah@usc.edu>
2023-04-06 01:49:53 -0700

    dropped unused models

--- app/schemas/data.py
+++ app/schemas/data.py
@@ -1,17 +1,4 @@
-from typing import Any, Dict, Optional
-
-from bson import ObjectId
-from pydantic import BaseModel, Field
-
-from app.db.utils import PyObjectId
-
-
-class DataModel(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    data: Any
-
-    class Config:
-        json_encoders = {ObjectId: str}
+from pydantic import BaseModel
 
 
 class PostDataResponse(BaseModel):
