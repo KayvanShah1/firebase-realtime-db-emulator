c48075467c45df0adf70de4b34b19b6689d406e2
Kayvan Shah <kayvan.shah@usc.edu>
2023-04-11 04:39:41 -0700

    added set & delete index routes

--- app/api/v2/api.py
+++ app/api/v2/api.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from app.api.v2.endpoints import retrieve_data, save_data, set_index
 
 api_router = APIRouter()
-api_router.include_router(save_data.router, tags=["Save Data"])
-api_router.include_router(retrieve_data.router, tags=["Retrieve Data"])
+api_router.include_router(save_data.router, tags=["Save Data v2"])
+api_router.include_router(retrieve_data.router, tags=["Retrieve Data v2"])
 api_router.include_router(set_index.router, tags=["Set Index"])
 # api_router.include_router(login.router, tags=["Authentication"])
