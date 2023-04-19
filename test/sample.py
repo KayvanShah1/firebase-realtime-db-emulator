c243750b0a66f0cf3c4884d4f81fc0a36ac0d548
Kayvan Shah <kayvan.shah@usc.edu>
2023-04-19 01:11:31 -0700

    arbitrary test

--- test/sample.py
+++ test/sample.py
@@ -9,5 +9,7 @@ client = MongoClient(os.getenv("MONGODB_URI"))
 db = client["firebase_db"]
 
 # Generic Collection
-base_collection = db["firebase_collection"]
-base_collection.insert_one({"x": 23})
+base_collection = db["sample-dummy"]
+doc = base_collection.find({}, {"_id": 0}).sort([("_fm_val", 1)])
+
+print(list(doc))
